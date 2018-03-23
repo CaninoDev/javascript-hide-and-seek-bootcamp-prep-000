6168cd704dd5610640325497ff55cfa29c46dc61
Ian Canino <ian.canino@gmail.com>
2018-03-23 21:46:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,45 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target')
+}
+
+function increaseRankBy(n) {
+  const rank = document.querySelectorAll('.ranked-list')
+  for (var i = 0; i < rank.length; i++) {
+    rank[i].innerHTML = parseInt(rank[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  /* Cheat answer?
+  *     var deep = document.getElementById('grand-node').querySelectorAll('div')
+  *     return deep[deep.length - 1]
+  */
+  
+  /* This function will return the deepest 'div' node relative to its parent 'grand-node' */
+  if(!document.getElementById('grand-node')) { // if no grand-node
+    return "No 'grand-node' found"
+  }
+  var grandNodeArr = []
+  var nodeDepth = 0
+  var nodeIndex = 0
+  var maybeDeepNode = 0
+  var maybeDeepIndex = 0
+  do {
+    grandNodeArr.push(document.getElementById('grand-node').querySelectorAll('div'))
+    if (grandNodeArr[nodeIndex].length > nodeDepth) {
+      nodeDepth = grandNodeArr[nodeIndex].length
+      maybeDeepNode = grandNodeArr[nodeIndex][nodeDepth - 1]
+      maybeDeepIndex = nodeIndex
+      document.getElementById('grand-node').id = nodeIndex
+      nodeIndex++
+    }
+  } while (document.getElementById('grand-node')) // while there are grand-nodes, push them on Array
+  document.getElementById(maybeDeepIndex).id = 'grand-node' // rename the node with the deepest <div> element back to grand-node
+  return maybeDeepNode
+}
+  
+  
